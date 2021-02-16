48ffbb6a0d47c57de866f3dfb6ae49b4c1ed7bb2
JamesOkemwa <okejames55@gmail.com>
2021-02-16 12:46:33 +0300

    Chat feed images bug fixing

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -2,7 +2,7 @@ const MyMessage = ({ message }) => {
     if(message?.attachments?.length > 0){
         return(
             <img 
-                src={message.attachments[0].files}
+                src={message.attachments[0].file}
                 alt='message-attachment'
                 className='message-image'
                 style={{ float: 'right'}}
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -12,7 +12,7 @@ const TheirMessage = ({ lastMessage, message }) => {
                 message?.attachments?.length > 0
                 ? (
                         <img 
-                            src={message.attachments[0].files}
+                            src={message.attachments[0].file}
                             alt='message-attachment'
                             className='message-image'
                             style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
